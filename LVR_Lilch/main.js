4adbad7a2e4f6062781b15ae9f1c0d57470276fb
lilch-mdm <71607978+lilch-mdm@users.noreply.github.com>
2020-12-06 00:21:49 -0500

    updates

---
+++ LVR_Lilch/main.js
@@ -0,0 +1,24 @@
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "50px";
+  }
+}
+
+
+/* Petition */ 
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
